00a2650f3cb7d76729d81dcf243d3de9b670cdd6
eternaleight <eternaleight0@gmail.com>
2022-05-15 11:43:22 +0900

    fix(*) feat new page & framer motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,96 +1,103 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>initial-commit</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <body className="px-2 container pt-1 mx-auto top-group bg-slate-200 pb-9 max-w-[1280px]">
-        <div className="flex gap-x-3">
-          <div className="box-group flex-[1]">
-            <h2 className='pt-4'>ワークフロー</h2>
-            <div>受注</div>
-            <div>ヒアリング</div>
-            <div>↓</div>
-            <div>調査分析</div>
-            <div className='p-1'>サイト設計(技術仕様書・サイトマップ)</div>
-            <div>↓</div>
-            <div className='p-1'>画面情報設計(ワイヤーフレーム設計)</div>
-            <div>↓</div>
-            <div>デザイン</div>
-            <div>↓</div>
-            <div>コーディング</div>
-            <div className='p-1'>プログラミング・cms業務</div>
-            <div>テストアップ</div>
-            <div>↓</div>
-            <div>納品</div>
-            <div>↓</div>
-            <div>運用</div>
-            <div>改修</div>
+      <motion.div
+        className=''
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, backgroundColor: '' }}
+        exit={{ opacity: 0 }}
+        // transition={{ ease: "easeOut", duration: 1 }}
+      >
+        <Head>
+          <title>initial-commit</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="flex flex-col p-2 gap-3">
+          <div className=''>
+        
+            <Link href="/workflow" scroll={false}>ワークフロー</Link>
           </div>
-          <div className=" box-group flex-[1]">
-            <h2 className='pt-2 media sm:pt-4'>発注者とのやり取り</h2>
-            <div>↓</div>
-            <div>打ち合わせ</div>
-            <div>見積もり提出</div>
-            <div>↓</div>
-            <div>↓</div>
-            <div>見積もり提出</div>
-            <div>↓</div>
-            <div>確認</div>
-            <div>↓</div>
-            <div>確認</div>
-            <div>↓</div>
-            <div>↓</div>
-            <div>↓</div>
-            <div>確認</div>
-            <div>↓</div>
-            <div>請求</div>
-            <div>↓</div>
-            <div>↓</div>
+          <div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
           </div>
-          <div className="box-group flex-[2]">
-            <h2 className='pt-4'>使用するツール</h2>
-            <section className="box3">
-              <div>メール 電話 クラウドソーシング</div>
-              <div>＜ヒアリングシート＞Execel メモ帳 ノート Googleスプレッドシート</div>
-              <div>↓</div>
-              <div>Web検索 現場下見 Google Analytycsを使ったサイトの分析</div>
-              <div>Power Point Execel</div>
-              <div>↓</div>
-              <div>
-                Adobe XD Figma Photoshop Sketch Excel Power Point Illustrator Googleスライド
-              </div>
-              <div>↓</div>
-              <div>Adobe XD Figma Photoshop Sketch Illustrator</div>
-              <div>↓</div>
-              <div className=''>
-                HTML CSS Typescript React Nect.js TailwindCSS
-                CMSを使って管理・更新するシステムをカスタマイズ Sanity.io Firebase MovableType
-                EC-CUBE
-              </div>
-              <div>↓</div>
-              <div>
-                FTPクライアントを経由してサーバーにデータをアップロード Dreamweaver winSCP Transmit
-                FileZilla FFFTP Cyber Duck
-              </div>
-              <div>↓</div>
-              <div>＜更新仕様書＞Power Point Word Illustrator</div>
-              <div>↓</div>
-              <div>＜分析レポート＞Google Analytics Google Search Console</div>
-              <div>↓</div>
-            </section>
+          <div>
+            <Link href="/workflow" scroll={false}>ワークフロー</Link>
           </div>
         </div>
-      </body>
-
-      <footer></footer>
+      </motion.div>
     </>
   )
 }
---
+++ pages/workflow.tsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+const workflow = () => (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, backgroundColor: ''}}
+        exit={{ opacity: 0 }}
+      >
+  <div className="rounded-[8px] shadow1 top-group container mx-auto max-w-[1280px] bg-slate-200 px-2 pt-1 pb-9">
+    <div className="flex gap-x-3">
+      <div className="box-group flex-[1]">
+        <Link href='/' scroll={false}>
+        <h2 className="pt-4 cursor-pointer">ワークフロー</h2>
+        </Link>
+        <div>受注</div>
+        <div>ヒアリング</div>
+        <div>↓</div>
+        <div>調査分析</div>
+        <div className="p-1">サイト設計(技術仕様書・サイトマップ)</div>
+        <div>↓</div>
+        <div className="p-1">画面情報設計(ワイヤーフレーム設計)</div>
+        <div>↓</div>
+        <div>デザイン</div>
+        <div>↓</div>
+        <div>コーディング</div>
+        <div className="p-1">プログラミング・cms業務</div>
+        <div>テストアップ</div>
+        <div>↓</div>
+        <div>納品</div>
+        <div>↓</div>
+        <div>運用</div>
+        <div>改修</div>
+      </div>
+      <div className=" box-group flex-[1]">
+        <h2 className="pt-2 media sm:pt-4">発注者とのやり取り</h2>
+        <div>↓</div>
+        <div>打ち合わせ</div>
+        <div>見積もり提出</div>
+        <div>↓</div>
+        <div>↓</div>
+        <div>見積もり提出</div>
+        <div>↓</div>
+        <div>確認</div>
+        <div>↓</div>
+        <div>確認</div>
+        <div>↓</div>
+        <div>↓</div>
+        <div>↓</div>
+        <div>確認</div>
+        <div>↓</div>
+        <div>請求</div>
+        <div>↓</div>
+        <div>↓</div>
+      </div>
+      <div className="box-group flex-[2]">
+        <h2 className="pt-4">使用するツール</h2>
+        <section className="box3">
+          <div>メール 電話 クラウドソーシング</div>
+          <div>＜ヒアリングシート＞Execel メモ帳 ノート Googleスプレッドシート</div>
+          <div>↓</div>
+          <div>Web検索 現場下見 Google Analytycsを使ったサイトの分析</div>
+          <div>Power Point Execel</div>
+          <div>↓</div>
+          <div>Adobe XD Figma Photoshop Sketch Excel Power Point Illustrator Googleスライド</div>
+          <div>↓</div>
+          <div>Adobe XD Figma Photoshop Sketch Illustrator</div>
+          <div>↓</div>
+          <div className="">
+            HTML CSS Typescript React Nect.js TailwindCSS
+            CMSを使って管理・更新するシステムをカスタマイズ Sanity.io Firebase MovableType EC-CUBE
+          </div>
+          <div>↓</div>
+          <div>
+            FTPクライアントを経由してサーバーにデータをアップロード Dreamweaver winSCP Transmit
+            FileZilla FFFTP Cyber Duck
+          </div>
+          <div>↓</div>
+          <div>＜更新仕様書＞Power Point Word Illustrator</div>
+          <div>↓</div>
+          <div>＜分析レポート＞Google Analytics Google Search Console</div>
+          <div>↓</div>
+        </section>
+      </div>
+    </div>
+  </div>
+        </motion.div>
+)
+
+export default workflow
