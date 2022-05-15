756612679e4b8f9e20d6682b487e7eff92e06fdb
eternaleight <eternaleight0@gmail.com>
2022-05-15 15:22:58 +0900

    feat(body): random color test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,53 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  const [color, setColor] = useState<any>('black')
+
+  type random = {
+    randomColor: () => string
+  }
+
+  useEffect(() => {
+    const randomColor = () => {
+      const math = Math.floor(Math.random() * 2)
+      switch (math) {
+        case 0:
+         return 'white'
+        case 1:
+         return 'aliceblue'
+      }
+    }
+    setColor(randomColor)
+    document.body.style.backgroundColor = color
+    document.body.style.transitionProperty = 'backgroundColor'
+  }, [color])
+
+  // useEffect(() => {
+  //   const randomColor = () => {
+  //     switch (color) {
+  //       case 0:
+  //         document.body.style.backgroundColor = 'red'
+  //         break
+  //       case 1:
+  //         document.body.style.backgroundColor = 'blue'
+  //         break
+  //       case 2:
+  //         document.body.style.backgroundColor = 'green'
+  //         break
+  //     }
+  //   }
+  //   console.log(randomColor)
+  // }, [color])
+
   return (
     <>
       <motion.div
-        className=''
+        className=""
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, backgroundColor: '' }}
+        animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         // transition={{ ease: "easeOut", duration: 1 }}
       >
@@ -20,81 +59,84 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className="flex flex-col p-2 gap-3">
-          <div className=''>
-        
-            <Link href="/workflow" scroll={false}>ワークフロー</Link>
+          <div className="">
+            <Link href="/workflow" scroll={false}>
+              ワークフロー
+            </Link>
           </div>
           <div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
           <div>
-            <Link href="/workflow" scroll={false}>ワークフロー</Link>
+            <Link href="/workflow" scroll={false}>
+              ワークフロー
+            </Link>
           </div>
         </div>
       </motion.div>
--- pages/workflow.tsx
+++ pages/workflow.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 const workflow = () => (
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, backgroundColor: ''}}
+        animate={{ opacity: 1}}
         exit={{ opacity: 0 }}
       >
   <div className="rounded-[8px] shadow1 top-group container mx-auto max-w-[1280px] bg-slate-200 px-2 pt-1 pb-9">
